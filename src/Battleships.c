45b3541378e0a98368471f6a1cf8f138121353c2
Ronen@Veksler <vekslerronen@gmail.com>
2025-05-03 18:02:07 +0300

    Ensure connection function work successfully

--- src/Battleships.c
+++ src/Battleships.c
@@ -1,12 +1,31 @@
 #include "Menu.h"   
+#include "Communication.h"
 
 /*
  * Compilation line:
-gcc -o Battleships src/Battleships.c src/IO_Assist.h src/IO_Assist.c src/Menu.h src/Menu.c src/Communication.h src/ src/Communication.c
+gcc -o Battleships src/Battleships.c src/IO_Assist.h src/IO_Assist.c src/Menu.h src/Menu.c src/Communication.h src/Communication.c -lws2_32 -liphlpapi
  */
 
 int
 main(void) {
-    printf("%d\n", get_menu_function());
+    SOCKET user_socket;
+    WSADATA wsa_data;
+    int menu_function = get_menu_function();
+    
+    if (WSAStartup(MAKEWORD(2, 2), &wsa_data)) {
+        PRINT_ERROR("WSAStartup failed");
+        return EXIT_FAILURE;
+    }
+
+    switch (menu_function) {
+        case JOIN_CODE:
+            connect_to_partner(&user_socket);
+            break;
+        case CREATE_CODE:
+            accept_partner(&user_socket);
+            break;
+        case EXIT_CODE:
+            break;
+    }
     return EXIT_SUCCESS;
 }
